808f39417e899e9746ab4cb81c2ad31ab11cd80e
MichalRais <Michal.Rais@iCloud.com>
2025-10-16 11:55:09 +0100

    feat: define ISS State and ISS Action types for useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 import LiveMap from "./components/LiveMap"
 
+interface ISSState {
+    latitude: number
+    longitude: number
+    timestamp: number
+    error: string | null    
+}
+
+type ISSAction =
+    | { type: "SET_DATA"; payload: { latitude: number; longitude: number; timestamp: number } }
+    | { type: "SET_ERROR"; payload: string}
+
 const App = () => {
   return (
     <div>App</div>
