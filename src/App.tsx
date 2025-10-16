b26c463094fbcdcc0e04b7912ff2d455549676dd
MichalRais <Michal.Rais@iCloud.com>
2025-10-16 11:49:25 +0100

    feat: import useState, useEffect, axios, and LiveMap in App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import LiveMap from "./components/LiveMap"
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
